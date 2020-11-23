2bcee1fba9890718bb656e75528180c67ca381a5
ijgnd <fooijgnd@barijgnd>
2020-11-23 22:47:54 +0100

    geany on MacOS

--- src/get_text_editor.py
+++ src/get_text_editor.py
@@ -22,6 +22,7 @@ def get_editor():
         "gedit",
     ]
     if sys.platform == "darwin":
+        editors.append("/Applications/Geany.app/Contents/MacOS/geany")
         editors.append("open -t")
     for editor in editors:
         command = find_executable(editor)
